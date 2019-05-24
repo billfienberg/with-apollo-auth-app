04432db5fdcdf3c4a8b9a1e96595d4aa4ab0860f
Bill Fienberg <billfienberg@gmail.com>
2019-05-24 17:52:03 -0400

    SigninBox.js: Apply formatting from prettier

--- components/SigninBox.js
+++ components/SigninBox.js
@@ -1,7 +1,7 @@
-import { Mutation, withApollo } from 'react-apollo'
-import gql from 'graphql-tag'
-import cookie from 'cookie'
-import redirect from '../lib/redirect'
+import { Mutation, withApollo } from "react-apollo";
+import gql from "graphql-tag";
+import cookie from "cookie";
+import redirect from "../lib/redirect";
 
 const SIGN_IN = gql`
   mutation Signin($email: String!, $password: String!) {
@@ -9,70 +9,70 @@ const SIGN_IN = gql`
       token
     }
   }
-`
+`;
 
 // TODO: Find a better name for component.
 const SigninBox = ({ client }) => {
-  let email, password
+  let email, password;
 
   return (
     <Mutation
       mutation={SIGN_IN}
       onCompleted={data => {
         // Store the token in cookie
-        document.cookie = cookie.serialize('token', data.signinUser.token, {
+        document.cookie = cookie.serialize("token", data.signinUser.token, {
           maxAge: 30 * 24 * 60 * 60 // 30 days
-        })
+        });
         // Force a reload of all the current queries now that the user is
         // logged in
         client.cache.reset().then(() => {
-          redirect({}, '/')
-        })
+          redirect({}, "/");
+        });
       }}
       onError={error => {
         // If you want to send error to external service?
-        console.log(error)
+        console.log(error);
       }}
     >
       {(signinUser, { data, error }) => (
         <form
           onSubmit={e => {
-            e.preventDefault()
-            e.stopPropagation()
+            e.preventDefault();
+            e.stopPropagation();
 
             signinUser({
               variables: {
                 email: email.value,
                 password: password.value
               }
-            })
+            });
 
-            email.value = password.value = ''
+            email.value = password.value = "";
           }}
         >
           {error && <p>No user found with that information.</p>}
           <input
-            name='email'
-            placeholder='Email'
+            name="email"
+            placeholder="Email"
             ref={node => {
-              email = node
+              email = node;
             }}
           />
           <br />
           <input
-            name='password'
-            placeholder='Password'
+            name="password"
+            placeholder="Password"
             ref={node => {
-              password = node
+              password = node;
             }}
-            type='password'
+            type="password"
           />
           <br />
           <button>Sign in</button>
         </form>
       )}
     </Mutation>
-  )
-}
+  );
+};
 
-export default withApollo(SigninBox)
+export default withApollo(SigninBox);
